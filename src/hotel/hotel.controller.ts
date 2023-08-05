e7e0637f066ebd19fed2b3a1b7ac4e4f13a82d50
Elua-b <bugingoeloi@gmail.com>
2023-08-05 12:43:15 +0200

    working on hotel module

---
+++ src/hotel/hotel.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Res } from "@nestjs/common";
+import { HotelService } from "./hotel.service";
+import { Request,Response } from "express";
+// import { Hotel } from "@prisma/client";
+import { Hotel } from "./hotel.model";
+@Controller('hotel')
+export class HotelController{
+    constructor(private readonly hotelService: HotelService) {}
+    @Get()
+    async getAllHotels(@Req() request:Request,@Res() response:Response):Promise<any>{
+        try {
+            const result = await this.hotelService.getAllHotels();
+            return response.status(200).json({
+                status:'ok!',
+                message:'successfully fetch data',
+                result:result
+            })
+        } catch (error) {
+            return response.status(500).json({
+                status:'error',
+                message:'Internal server error',
+            })
+        }
+    }
+    @Post()
+    async postHotel(@Body() postData:Hotel):Promise<Hotel>{
+        return this.hotelService.createHotel(postData);
+    }
+    @Get('/:hotel_id')
+    async getHotel(@Param('hotel_id') hotel_id:number):Promise<Hotel|null>{
+        return this.hotelService.getHotel(hotel_id);
+    }
+    @Delete('/:hotel_id')
+    async deleteHotel(@Param('hotel_id') hotel_id:string):Promise<Hotel>{
+        return this.hotelService.deleteHotel(hotel_id);
+    }
+    @Put('/:hotel_id')
+    async updateHotel(@Param('hotel_id') hotel_id:string,@Body() postData:Hotel):Promise<Hotel>{
+        return this.hotelService.updateHotel(hotel_id,postData);
+    }
+}
