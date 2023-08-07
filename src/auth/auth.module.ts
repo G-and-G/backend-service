5500c726cd19da08e0fbaf79821411fd23f9ef21
Elua-b <bugingoeloi@gmail.com>
2023-08-07 12:34:23 +0200

    adding middle wares in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {JwtModule} from '@nestjs/jwt'
+import { RequestLoggingMiddleware } from 'src/middlewares/requestLogging.middleware';
+import { ErrorHandlingMiddleware } from 'src/middlewares/ErrorHandling.middleware';
+import { AuthorizationMiddleware } from 'src/middlewares/authorisation.middleware';
 
 @Module({
   imports:[JwtModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule  implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestLoggingMiddleware)
+      .forRoutes('*');
+
+    consumer
+      .apply(ErrorHandlingMiddleware)
+      .forRoutes('*');
+
+    consumer
+      .apply(AuthorizationMiddleware)
+      .forRoutes('auth/protected-route');
+  }
+}
