d2957eda752ee72450925e63a1722984154c6c3c
Hallcoder <hallcoder25@gmail.com>
2023-08-09 17:01:30 +0200

    [fix]: error handling middleware

--- src/middlewares/ErrorHandling.middleware.ts
+++ src/middlewares/ErrorHandling.middleware.ts
@@ -3,15 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class ErrorHandlingMiddleware implements NestMiddleware {
-    
-  use(err: any, req: Request, res: Response, next: NextFunction) {
+  use(req: Request, res: Response, next: NextFunction, err?: any) {
     if (err instanceof HttpException) {
       return res.status(err.getStatus()).json({
         message: err.message,
       });
     }
 
-    console.error(err); 
+    console.error(err);
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       message: 'Internal Server Error',
     });
