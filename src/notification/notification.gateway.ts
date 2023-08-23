e4c5ab0df3fb4ec17c83db8df1963c45615ae9f5
Hallcoder <hallcoder25@gmail.com>
2023-08-23 08:55:55 +0200

    order

--- src/notification/notification.gateway.ts
+++ src/notification/notification.gateway.ts
@@ -1,13 +1,14 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server , Socket} from 'socket.io';
 
-@WebSocketGateway({ url: 'ws://localhost:3000' })
+@WebSocketGateway({ url: 'ws://localhost:3000',cors:true })
 export class NotificationGateway {
   @WebSocketServer()
   server:Server;
   
   handleConnection(client:Socket){
     console.log("New client",client.id);
+    
   }
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
