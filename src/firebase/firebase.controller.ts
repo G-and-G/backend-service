a4620b615232b9d3bdaa60fcc95a99fe120a3820
Hallcoder <hallcoder25@gmail.com>
2023-08-25 18:23:25 +0200

    response

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Param, Post, Res } from '@nestjs/common';
+import ApiResponse from 'src/utils/ApiResponse';
 
 @Controller('firebase')
 export class FirebaseController {
   @Post('/:token')
-  async receiveToken(@Param('token') token: string) {
+  async receiveToken(@Param('token') token: string, @Res() res) {
+   try {
     process.env['FIREBASE_TOKEN'] = token;
     console.log(process.env.FIREBASE_TOKEN);
+    return ApiResponse.success("Token received",null,200);
+   } catch (error) {
+    return ApiResponse.error("Something went wrong" + error.message,null,error.status)
+   }
+
   }
 }
