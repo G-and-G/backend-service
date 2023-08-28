5a6224163ba19f8a87f3f884ed1344471ccf4be0
Hallcoder <hallcoder25@gmail.com>
2023-08-28 17:09:37 +0200

    push

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,6 +1,6 @@
+import { PrismaClient } from '@prisma/client';
 import admin from 'firebase-admin';
-
-const serviceAccount = require(`/opt/render/project/grabngo-a3844-firebase-adminsdk-ftno0-c4b08dfd02.json`);
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
