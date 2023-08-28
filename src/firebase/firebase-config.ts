f66937c3701a641efc0bd2c409e29fc4768aed63
Hallcoder <hallcoder25@gmail.com>
2023-08-28 14:59:55 +0200

    debug 3

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require(`/opt/render/project/src/grabngo-a3844-firebase-adminsdk-ftno0-c4b08dfd02.json`);
+const serviceAccount = require(`/opt/render/project/grabngo-a3844-firebase-adminsdk-ftno0-c4b08dfd02.json`);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
