10f858b1be38c088a4dc32c825ff943833439dd8
Hallcoder <hallcoder25@gmail.com>
2024-08-29 10:42:01 +0200

    fix: error injection

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HotelService } from 'src/modules/hotel/hotel.service';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [OrderController],
   providers: [OrderService, HotelService],
   exports: [OrderService],
